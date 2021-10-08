ba3584840aa348dfbe62ceb843e52a6760b80b80
Xiangjun.Wu <s51043@edu.campus02.at>
2021-10-08 19:58:52 +0200

    variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,18 @@
 public class Calculator {
     public static void main(String[] args) {
+        int a = 111;
+        System.out.println(a);
 
+        double b = 2.2;
+        System.out.println(b);
+
+        boolean c = true;
+        System.out.println(c);
+
+        char d = 44;
+        System.out.println(d);
+
+        String e = "Text";
+        System.out.println(e);
     }
 }
